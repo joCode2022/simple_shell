c894b80e43332419f89714de30515b47bae6eab7
badr <badrelhoudni12@gmail.com>
2023-07-26 11:43:44 +0100

    .

--- realloc.c
+++ realloc.c
@@ -1,45 +1,63 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * _realloc - Reallocates a memory block using malloc and free
- * @ptr:pointer to previously allocated memory
- * @old_size: previous allocated size
- * @new_size: new allocated size
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
  *
- * Return: a pointer to the new allocated memory, returns NULL if an error.
+ * Return: pointer to da ol'block nameen.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new;
+	char *p;
 
-	if (old_size == new_size)
-	{
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
 		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-		return (ptr);
-	}
-	if (ptr != NULL && new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
 
-	new = malloc(new_size);
-	if (new == NULL)
+	p = malloc(new_size);
+	if (!p)
 		return (NULL);
 
-	if (new_size < old_size)
-	{
-		_memcpy(new, ptr, new_size);
-		free(ptr);
-	}
-	else if (new_size > old_size)
-	{
-		_memcpy(new, ptr, old_size);
-		free(ptr);
-	}
-	return (new);
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
 }
+
